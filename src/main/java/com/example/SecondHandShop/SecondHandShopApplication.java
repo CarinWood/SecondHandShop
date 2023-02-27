5ff2278aea4860b993dd39c8f61e029997e66f3f
Carin Wood <carin.wood@brightstraining.com>
2023-02-27 21:06:25 +0100

    added a header to shop.html file

--- src/main/java/com/example/SecondHandShop/SecondHandShopApplication.java
+++ src/main/java/com/example/SecondHandShop/SecondHandShopApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SecondHandShopApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SecondHandShopApplication.class, args);
 	}
 
