4c8d44c693239cf18335739f366b366f33d7b6de
RobinAlfengård <robin.alfengard@hotmail.com>
2023-03-01 13:05:28 +0100

    added test - robin

--- src/main/java/com/example/SecondHandShop/SecondHandShopApplication.java
+++ src/main/java/com/example/SecondHandShop/SecondHandShopApplication.java
@@ -8,8 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SecondHandShopApplication {
 
 
-
-
 	public static void main(String[] args) {
 
 		SpringApplication.run(SecondHandShopApplication.class, args);
--- src/test/java/com/example/SecondHandShop/SecondHandShopApplicationTests.java
+++ src/test/java/com/example/SecondHandShop/SecondHandShopApplicationTests.java
@@ -1,7 +1,10 @@
 package com.example.SecondHandShop;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
 
 @SpringBootTest
 class SecondHandShopApplicationTests {
@@ -10,4 +13,22 @@ class SecondHandShopApplicationTests {
 	void contextLoads() {
 	}
 
+	@Test
+	public void getSumOfCustomerList(){
+		Cart cart = new Cart();
+		ArrayList<Product> list = cart.getItems();
+		list.add(new Product("Product1", "Desc1", 20, "image", Cathegory.HEM, 20));
+		list.add(new Product("Product2", "Desc2", 20, "image", Cathegory.HEM, 20));
+		list.add(new Product("Product3", "Desc3", 20, "image", Cathegory.HEM, 20));
+		Assertions.assertEquals(60, cart.getSum());
+
+		//add 120 to total price
+		list.add(new Product("Product4", "Desc4", 120, "image", Cathegory.HEM, 20));
+		Assertions.assertEquals(180, cart.getSum());
+
+		//remove product with price 20
+		list.remove(1);
+		Assertions.assertEquals(160, cart.getSum());
+
+	}
 }
